e0e60c51d0a7a29b505b69a9ac87384fb39c8e40
jessemusic <jmc_cursos@hotmail.com>
2021-08-11 23:25:39 -0300

    treinamento GAMA anotações 2 estado

--- gamaReactProduction/gamagitjesse/src/App.js
+++ gamaReactProduction/gamagitjesse/src/App.js
@@ -1,11 +1,8 @@
 import React, { useState } from 'react';
 
-
 function App(props) {
-  const [ usuario, setUsuario ] = useState('Jesse');
   return (
     <>
-    <p>{ usuario } </p>
     <input name="usuario" id="usuario" className="usuarioInput" placeholder="Usuário"  />
     </>
   );
